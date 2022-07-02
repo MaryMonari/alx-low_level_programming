73caf6f91971a0ab75353e83ee52a14dda3b380f
MaryMonari <moraamarymonari@gmail.com>
2022-07-01 18:56:31 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * prints exactly and that piece of art is useful followed by a new line
+ * Return: end program
+ */
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", n);
+
+	return (1);
+}
